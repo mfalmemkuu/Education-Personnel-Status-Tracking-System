43cdd753ee997f786fd5e81ac9718f320d458d0f
Sarah Laurin <sarahno.laurin@gmail.com>
2023-08-06 18:45:53 -0400

    updating facility progress with extra table checks

--- php/facility/create-view.php
+++ php/facility/create-view.php
@@ -12,7 +12,15 @@
         Province: <input type="text" name="province"><br>
         Phone Number: <input type="text" name="phoneNumber"><br>
         Web Address: <input type="text" name="webaddress"><br>
-        
+        Facility Type: <select name="type">
+                <option value="generalManagementFacilities">General Management Facility</option>
+                <option value="headOfficeFacilities">Head Office Facility</option>
+                <option value="primarySchools">Primary School</option>
+                <option value="middleSchools">Middle School</option>
+                <option value="highSchools">High School</option>
+            </select>
+        Ministry Name: <input type="text" name="ministryName"><br>    
+        <br>
         <br>
         <input type="submit">
     </form><br>
