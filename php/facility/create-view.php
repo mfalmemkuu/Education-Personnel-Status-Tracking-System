40f49f3f4db4ca1d853c5731ec13ce07cc6d26fb
Sarah Laurin <sarahno.laurin@gmail.com>
2023-08-06 19:18:37 -0400

    facilities crud operations mostly done

--- php/facility/create-view.php
+++ php/facility/create-view.php
@@ -5,22 +5,10 @@
     <h1>Add a Facility</h1>
     <form action="facility-c.php">
         Name: <input type="text" name="fname"><br>        
-        Capacity: <input type="text" name="capacity"><br>       
-        Address: <input type="text" name="adress"><br>
-        City: <input type="text" name="city"><br>
+        Capacity: <input type="text" name="capacity"><br>    
         PostalCode: <input type="text" name="postalCode"><br>
-        Province: <input type="text" name="province"><br>
         Phone Number: <input type="text" name="phoneNumber"><br>
         Web Address: <input type="text" name="webaddress"><br>
-        Facility Type: <select name="type">
-                <option value="generalManagementFacilities">General Management Facility</option>
-                <option value="headOfficeFacilities">Head Office Facility</option>
-                <option value="primarySchools">Primary School</option>
-                <option value="middleSchools">Middle School</option>
-                <option value="highSchools">High School</option>
-            </select>
-        Ministry Name: <input type="text" name="ministryName"><br>    
-        <br>
         <br>
         <input type="submit">
     </form><br>
