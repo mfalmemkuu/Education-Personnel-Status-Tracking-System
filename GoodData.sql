49dc35aa1447a88c84e239bb41780f43594fd215
mathieutrudeau <46968018+mathieutrudeau@users.noreply.github.com>
2023-08-07 11:19:49 -0400

    Create GoodData.sql

---
+++ GoodData.sql
@@ -0,0 +1,12 @@
+-- Insert Ministries
+INSERT INTO Ministries (MinistryID, Name) VALUES
+(1, 'Ministry of Education - Alberta'),
+(2, 'Ministry of Education - British Columbia'),
+(3, 'Ministry of Education - Manitoba'),
+(4, 'Ministry of Education - New Brunswick'),
+(5, 'Ministry of Education - Newfoundland and Labrador'),
+(6, 'Ministry of Education - Nova Scotia'),
+(7, 'Ministry of Education - Ontario'),
+(8, 'Ministry of Education - Prince Edward Island'),
+(9, 'Ministry of Education - Quebec'),
+(10, 'Ministry of Education - Saskatchewan');
