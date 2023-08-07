dccfef76e85c5447b87f2882e8a13f196c385532
mathieutrudeau <46968018+mathieutrudeau@users.noreply.github.com>
2023-08-07 11:37:45 -0400

    Update GoodData.sql

--- GoodData.sql
+++ GoodData.sql
@@ -1,12 +1,67 @@
--- Insert Ministries
-INSERT INTO Ministries (MinistryID, Name) VALUES
-(1, 'Ministry of Education - Alberta'),
-(2, 'Ministry of Education - British Columbia'),
-(3, 'Ministry of Education - Manitoba'),
-(4, 'Ministry of Education - New Brunswick'),
-(5, 'Ministry of Education - Newfoundland and Labrador'),
-(6, 'Ministry of Education - Nova Scotia'),
-(7, 'Ministry of Education - Ontario'),
-(8, 'Ministry of Education - Prince Edward Island'),
-(9, 'Ministry of Education - Quebec'),
-(10, 'Ministry of Education - Saskatchewan');
+-- Ministry of Education - Quebec
+INSERT INTO Ministries (MinistryID, Name) VALUES (3, 'Ministry of Education - Quebec');
+
+-- Ministry of Education - Quebec Facilities
+INSERT INTO Facilities (Name, WebAddress, Capacity, PostalCode, PhoneNumber) VALUES
+('Quebec Education Headquarters', 'www.educationquebec.gouv.qc.ca', 500, 'H2Y 1T1', '514-555-1111');
+INSERT INTO HeadOfficeFacilities (FacilityID) VALUES (LAST_INSERT_ID());
+
+INSERT INTO Facilities (Name, WebAddress, Capacity, PostalCode, PhoneNumber) VALUES
+('General Management Facility 1 - Quebec', 'www.gmf1-quebec.ca', 200, 'H2Y 2S1', '514-555-2222'),
+('General Management Facility 2 - Quebec', 'www.gmf2-quebec.ca', 180, 'H2Y 3T1', '514-555-3333');
+INSERT INTO GeneralManagementFacilities (FacilityID) VALUES (LAST_INSERT_ID() - 1), (LAST_INSERT_ID());
+
+INSERT INTO Facilities (Name, WebAddress, Capacity, PostalCode, PhoneNumber) VALUES
+('John Renny High School - Quebec', 'www.johnrennyhigh-quebec.ca', 800, 'H3A 1T1', '514-555-4444'),
+('Quebec Elementary School - Quebec', 'www.quebecele-quebec.ca', 500, 'H3B 2T2', '514-555-5555'),
+('Maplewood Middle School - Quebec', 'www.maplewoodmiddle-quebec.ca', 600, 'H3C 1T3', '514-555-6666'),
+('Westminster Primary School - Quebec', 'www.westminsterprimary-quebec.ca', 400, 'H3A 1W1', '514-555-7777'),
+('Rosehill Secondary School - Quebec', 'www.rosehillsecondary-quebec.ca', 700, 'H3B 2W2', '514-555-8888'),
+('Quebec High School - Quebec', 'www.quebechigh-quebec.ca', 900, 'H3C 1W3', '514-555-9999'),
+('Greenwood Middle School - Quebec', 'www.greenwoodmiddle-quebec.ca', 600, 'H3A 1X1', '514-555-0000'),
+('Hilltop Elementary School - Quebec', 'www.hilltopelem-quebec.ca', 550, 'H3B 2X2', '514-555-1010'),
+('Northview Secondary School - Quebec', 'www.northviewsecondary-quebec.ca', 750, 'H3C 1X3', '514-555-2020'),
+('Quebec East High School - Quebec', 'www.quebeceasthigh-quebec.ca', 800, 'H3A 1Y1', '514-555-3030');
+INSERT INTO EducationalFacilities (FacilityID) VALUES 
+(LAST_INSERT_ID() - 9), (LAST_INSERT_ID() - 8), (LAST_INSERT_ID() - 7), (LAST_INSERT_ID() - 6), (LAST_INSERT_ID() - 5),
+(LAST_INSERT_ID() - 4), (LAST_INSERT_ID() - 3), (LAST_INSERT_ID() - 2), (LAST_INSERT_ID() - 1), LAST_INSERT_ID();
+
+-- Ministry of Education - Quebec Postal Codes
+INSERT INTO Addresses_facilities (PostalCode, Province, Address, City) VALUES
+('H2Y 1T1', 'Quebec', '1234 St-Laurent Blvd', 'Montreal'),
+('H2Y 2S1', 'Quebec', '5678 Sherbrooke St', 'Montreal'),
+('H2Y 3T1', 'Quebec', '9101 St-Catherine St', 'Montreal'),
+('H3A 1T1', 'Quebec', '2222 University St', 'Montreal'),
+('H3B 2T2', 'Quebec', '3333 Drummond St', 'Montreal'),
+('H3C 1T3', 'Quebec', '4444 Peel St', 'Montreal'),
+('H3A 1W1', 'Quebec', '5555 Park Ave', 'Montreal'),
+('H3B 2W2', 'Quebec', '6666 Bishop St', 'Montreal'),
+('H3C 1W3', 'Quebec', '7777 Guy St', 'Montreal'),
+('H3A 1X1', 'Quebec', '8888 Mont-Royal Ave', 'Montreal'),
+('H3B 2X2', 'Quebec', '9999 St-Denis St', 'Montreal'),
+('H3C 1X3', 'Quebec', '1010 Maisonneuve St', 'Montreal'),
+('H3A 1Y1', 'Quebec', '1212 Peel St', 'Montreal');
+
+-- Ministry of Education - Quebec Operates
+INSERT INTO Operates (FacilityID, MinistryID) VALUES
+(43, 3),
+(44, 3),
+(45, 3),
+(46, 3),
+(47, 3),
+(48, 3),
+(49, 3),
+(50, 3),
+(51, 3),
+(52, 3),
+(53, 3),
+(54, 3),
+(55, 3),
+(56, 3),
+(57, 3),
+(58, 3),
+(59, 3),
+(60, 3),
+(61, 3),
+(62, 3),
+(63, 3);
