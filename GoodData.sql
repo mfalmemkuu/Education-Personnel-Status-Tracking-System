e52a39ee885882a487c5197e0587a9f3da26d02f
mathieutrudeau <46968018+mathieutrudeau@users.noreply.github.com>
2023-08-07 11:44:02 -0400

    Update GoodData.sql

--- GoodData.sql
+++ GoodData.sql
@@ -22,9 +22,9 @@ INSERT INTO Facilities (Name, WebAddress, Capacity, PostalCode, PhoneNumber) VAL
 ('Hilltop Elementary School - Quebec', 'www.hilltopelem-quebec.ca', 550, 'H3B 2X2', '514-555-1010'),
 ('Northview Secondary School - Quebec', 'www.northviewsecondary-quebec.ca', 750, 'H3C 1X3', '514-555-2020'),
 ('Quebec East High School - Quebec', 'www.quebeceasthigh-quebec.ca', 800, 'H3A 1Y1', '514-555-3030');
-INSERT INTO EducationalFacilities (FacilityID) VALUES 
-(LAST_INSERT_ID() - 9), (LAST_INSERT_ID() - 8), (LAST_INSERT_ID() - 7), (LAST_INSERT_ID() - 6), (LAST_INSERT_ID() - 5),
-(LAST_INSERT_ID() - 4), (LAST_INSERT_ID() - 3), (LAST_INSERT_ID() - 2), (LAST_INSERT_ID() - 1), LAST_INSERT_ID());
+-- Ministry of Education - Quebec Educational Facilities
+INSERT INTO EducationalFacilities (FacilityID)
+SELECT FacilityID FROM Facilities ORDER BY FacilityID DESC LIMIT 10;
 
 -- Ministry of Education - Quebec Postal Codes
 INSERT INTO Addresses_facilities (PostalCode, Province, Address, City) VALUES
